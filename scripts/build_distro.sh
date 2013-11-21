6fb91221d732863170fc66adf0a06fc164888534
Jakob Perry <japerry@45640.no-reply.drupal.org>
2013-11-21 01:56:33 -0500

    Tweaks to build_distro.sh -- change to docroot and use tar instead of extracting directly into directory.

--- scripts/build_distro.sh
+++ scripts/build_distro.sh
@@ -45,14 +45,15 @@ release_notes() {
 build_distro() {
     if [[ -d $BUILD_PATH ]]; then
         cd $BUILD_PATH
-        #tar -czvf $BUILD_PATH/sites.tar.gz publish/sites
-        sudo rm -rf ./publish
+        #tar -czvf $BUILD_PATH/sites.tar.gz docroot/sites
+        sudo rm -rf ./docroot
         # do we have the profile?
         if [[ -d $BUILD_PATH/cod_profile ]]; then
           if [[ -d $BUILD_PATH/repos ]]; then
             sudo rm -f /tmp/cod.tar.gz
+            sudo rm -f /tmp/docroot.tar.gz
             drush make --no-cache --no-core --contrib-destination --tar $BUILD_PATH/cod_profile/drupal-org.make /tmp/cod
-            drush make --no-cache --prepare-install --drupal-org=core $BUILD_PATH/cod_profile/drupal-org-core.make ./publish
+            drush make --no-cache --prepare-install --drupal-org=core $BUILD_PATH/cod_profile/drupal-org-core.make $BUILD_PATH/docroot
           else
             mkdir $BUILD_PATH/repos
             mkdir $BUILD_PATH/repos/modules
@@ -77,20 +78,22 @@ build_distro() {
             done
             build_distro $BUILD_PATH
           fi
+          # untar the docroot
+          cd $BUILD_PATH
           # symlink the profile to our dev copy
-          chmod -R 777 $BUILD_PATH/publish/sites/default
-          rm -rf $BUILD_PATH/publish/profiles/cod
-          cd $BUILD_PATH/publish/profiles
+          chmod -R 777 $BUILD_PATH/docroot/sites/default
+          rm -rf $BUILD_PATH/docroot/profiles/cod
+          cd $BUILD_PATH/docroot/profiles
           tar -zxvf /tmp/cod.tar.gz
-          chmod -R 775 $BUILD_PATH/publish/profiles/cod
-          rm -rf $BUILD_PATH/publish/profiles/cod/modules/contrib/cod*
-          rm -rf $BUILD_PATH/publish/profiles/cod/themes/contrib/cod*
-          ln -sfv $BUILD_PATH/repos/modules/cod* $BUILD_PATH/publish/profiles/cod/modules/contrib/
-          chmod -R 777 $BUILD_PATH/publish/sites/default
+          chmod -R 775 $BUILD_PATH/docroot/profiles/cod
+          rm -rf $BUILD_PATH/docroot/profiles/cod/modules/contrib/cod*
+          rm -rf $BUILD_PATH/docroot/profiles/cod/themes/contrib/cod*
+          ln -sfv $BUILD_PATH/repos/modules/cod* $BUILD_PATH/docroot/profiles/cod/modules/contrib/
+          chmod -R 777 $BUILD_PATH/docroot/sites/default
           # symlink the profile to our dev copy
-          rm -f $BUILD_PATH/publish/profiles/cod/*.*
-          rm -rf $BUILD_PATH/publish/profiles/cod/images
-          ln -s $BUILD_PATH/cod_profile/* $BUILD_PATH/publish/profiles/cod/
+          rm -f $BUILD_PATH/docroot/profiles/cod/*.*
+          rm -rf $BUILD_PATH/docroot/profiles/cod/images
+          ln -s $BUILD_PATH/cod_profile/* $BUILD_PATH/docroot/profiles/cod/
         else
           git clone http://git.drupal.org/project/cod.git cod_profile
           build_distro $BUILD_PATH
@@ -110,12 +113,12 @@ update() {
     # do we have the profile?
     if [[ -d $DOCROOT/profiles/cod ]]; then
       # do we have an installed cod profile?
-        sudo rm -f /tmp/publish.tar.gz
+        sudo rm -f /tmp/docroot.tar.gz
         sudo rm -f /tmp/cod.tar.gz
-        drush make --tar --drupal-org=core profiles/cod/drupal-org-core.make /tmp/publish
+        drush make --tar --drupal-org=core profiles/cod/drupal-org-core.make /tmp/docroot
         drush make --tar --drupal-org profiles/cod/drupal-org.make /tmp/cod
         cd ..
-        tar -zxvf /tmp/publish.tar.gz
+        tar -zxvf /tmp/docroot.tar.gz
         cd $DOCROOT/profiles/modules/contrib
         # remove the symlinks in the repos before we execute
         find . -mindepth 2 -type l | awk -F/ '{print $5}' | sed '/^$/d' > /tmp/repos.txt
